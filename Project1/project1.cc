3dd7d0b0e31cb241cb61b49a17c448790e905990
Brian Bongermino <firenight485@gmail.com>
2022-08-30 15:06:20 -0400

    adding tester

--- Project1/project1.cc
+++ Project1/project1.cc
@@ -5,16 +5,19 @@ using std::cout;
 using std::cin;
 using std::endl;
 int main() {
-    int input, base;
+    int input, base, n;
     int unique_digits = 0;
 
-    cout << "Please input a integer and a base" << endl;
     cin >> input >> base;
     //  Checks if the base is less than 9 or
     //  if the base is less than 2
-    if (base > 9 || base < 2) {
-        cout << "Base not accepted" << endl;
+    if (base < 9 || base > 2) {
+        cout << "sample text";
     }
-
+    //  Checks if the base is less than 9 or
+    //  if the base is less than 2
+    if (base > 9 || base < 2) {
+        cout << "Base Not Accepted" << endl;
+    }
     return 0;
 }
