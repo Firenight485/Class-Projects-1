bb599c390a89856f4f5459294c9a1a3ffaa847cf
Brian Bongermino <firenight485@gmail.com>
2022-09-01 10:57:36 -0400

    progress on splitting

--- Project1/project1.cc
+++ Project1/project1.cc
@@ -1,18 +1,25 @@
 // Copyright 2022 Brian Bongermino
 
 #include<iostream>
+#include<cmath>
 using std::cout;
 using std::cin;
 using std::endl;
 int main() {
-    int input, base, n;
+    int input, base;
     int unique_digits = 0;
 
     cin >> input >> base;
     //  Checks if the base is less than 9 or
     //  if the base is less than 2
-    if (base < 9 || base > 2) {
-        cout << "sample text";
+    if (base <= 9 || base >= 2) {
+        for (int i = 0; i < 10; ++i) {
+            int split =  input % 10;
+            input /= 10;
+
+            std::cout << split << endl;
+        }
+        //  cout << pow() << endl;
     }
     //  Checks if the base is less than 9 or
     //  if the base is less than 2
