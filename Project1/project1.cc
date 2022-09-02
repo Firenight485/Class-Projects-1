80d951d9d3d221e3959a5f87950970adff4c20c9
Brian Bongermino <firenight485@gmail.com>
2022-09-02 13:54:41 -0400

    pushing files

--- Project1/project1.cc
+++ Project1/project1.cc
@@ -6,17 +6,37 @@ using std::cout;
 using std::cin;
 using std::endl;
 int main() {
-    int input, base, split;
+    int input, base, split, temp, remainder;
+    int reversed_input = 0;
     int output = 0;
+    int raise = 0;
+    int count = 0;
 
-    cin >> input >> base;
+    cin >> input;
     //  Checks if the base is less than 9 or
     //  if the base is less than 2
-    if (base <= 9 || base >= 2) {
-        //  output for positive
-        while (input > 0 || input < 0) {
+    while (input != 0) {
+        remainder = input%10;
+        reversed_input = reversed_input*10+remainder;
+        input/10;
+    }
+    return reversed_input;
+
+    /*temp = input;
+
+    while (temp != 0) {
+        count++;
+        temp /=10;
+    }
+    raise = count;
+    if (base < 10 || base > 1) {
+        /**
+         * @brief splits and raises the input 
+         * 
+         */
+        /*while (input > 0) {
             split = input % 10;
-            output = output + (split*pow(base, 2));
+            output = output + split*pow(base, --raise);
             input = input / 10;
         }
             cout << output << endl;
@@ -25,6 +45,6 @@ int main() {
     //  if the base is less than 2
     if (base > 9 || base < 2) {
         cout << "Base Not Accepted" << endl;
-    }
+    }*/
     return 0;
 }
