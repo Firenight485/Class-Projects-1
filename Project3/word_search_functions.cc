02e80b918567b2d23ffdaf5dc33e2bece7dc5fd0
Brian Bongermino <firenight485@gmail.com>
2022-10-10 11:52:42 -0400

    cpplint

--- Project3/word_search_functions.cc
+++ Project3/word_search_functions.cc
@@ -8,8 +8,10 @@ using std::cout;
 using std::endl;
 #include<string>
 
-// Reads in a file line by line and checks if the character count is at least the constant
-// Ksize * Ksize, if it is is then the file is read into the char word_search and implemented
+// Reads in a file line by line and checks if the character count is
+// at least the constant
+// Ksize * Ksize, if it is is then the file is
+// read into the char word_search and implemented
 // in program 3
 bool ReadWordSearch(string file_name, char word_search[][kSize]) {
     ifstream file_in;
@@ -59,7 +61,7 @@ void PrintWordSearch(const char print_grid[][kSize]) {
 //  Finds a word from the grid going from left to right
 Position FindWordRight(const char find_right[][kSize], string to_find) {
     Position Location;
-    // sets the default position to -1,-1 (being out of the grid as a 
+    // sets the default position to -1,-1 (being out of the grid as a
     // as a way to set a default not found position)
     Location.col = -1;
     Location.row = -1;
@@ -73,7 +75,7 @@ Position FindWordRight(const char find_right[][kSize], string to_find) {
                 for (size_t l = 0; l < to_find.length(); l++) {
                 //  first checks if the size of the word fits in the grid
                 //  if it does then it finds the location of the pattern
-                //  ie if the word is program it will go over and find 
+                //  ie if the word is program it will go over and find
                 //  the length and if what it found fits and is equal
                 //  to the length of what to find's length is
                 //  it'll move on and check if it doesn't end on the starting
@@ -85,7 +87,7 @@ Position FindWordRight(const char find_right[][kSize], string to_find) {
                 }
                 }
                 }
-                //  outputs the location to the console 
+                //  outputs the location to the console
                 if (k == to_find.length()) {
                     Location.col = j;
                     Location.row = i;
@@ -113,7 +115,7 @@ Position FindWordLeft(const char find_left[][kSize], string to_find) {
                 //  checks for the size of to_find
                 for (size_t l = 0; l < to_find.length(); l++) {
                 //  j-l moves through the rows right to left and compares
-                //  what if found with the length of to_find. If it's 
+                //  what if found with the length of to_find. If it's
                 //  valid it'll add to k++
                 if ((j-l) < kSize && find_left[i][j - l] == to_find[l]) {
                     if (find_left[i][j] != '\0') {
@@ -176,7 +178,7 @@ Position FindWordUp(const char find_up[][kSize], string to_find) {
     //  default location
     Location.col = -1;
     Location.row = -1;
-    //  moves through row by row and column by column 
+    //  moves through row by row and column by column
     for (int i = 0; i < kSize; i++) {
         for (int j = 0; j < kSize; j++) {
                 // starts the pattern
@@ -192,7 +194,7 @@ Position FindWordUp(const char find_up[][kSize], string to_find) {
                 }
                 }
                 }
-                //  outputs found location to console 
+                //  outputs found location to console
                 if (k == to_find.length()) {
                     Location.col = j;
                     Location.row = i;
@@ -220,7 +222,7 @@ Position FindWordDiagonal(const char find_diag[][kSize], string to_find) {
                 // checks for length
                 for (size_t l = 0; l < to_find.length(); l++) {
                 // checks right both column direction and row
-                // direction are valid. If they are then it 
+                // direction are valid. If they are then it
                 // adds to k
                 if (((j+l) < kSize && (i+l) < kSize)
                 && find_diag[i + l][j + l] == to_find[l]) {
